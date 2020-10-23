6b082229bb57cf0b90b8fe21cea676dcfaafd3d8
Aleksandra <alex.shalimova@gmail.com>
2020-10-23 10:30:39 +0200

    Delete by ID Closes gh-7

--- index.js
+++ index.js
@@ -77,6 +77,39 @@ function validateCourse(course) {
     
 }
 
+// delete by ID
+
+app.delete('/data/courses.json/:id', (req, res) => {
+    // read the file system and look up the course by ID
+    // if doesn't exist - return 404
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            return console.error(err);
+		}
+		
+        let myCourses = JSON.parse(data);
+		const removeCourse = myCourses.find((c) => c.id === parseInt(req.params.id));
+		
+		if (!removeCourse) return res.status(404).send('The course with the given ID was not found');
+        
+        // delete
+        console.log('this will be deleted ', removeCourse);
+        const index = myCourses.indexOf(removeCourse);
+        myCourses.splice(index, 1);
+		res.send(removeCourse);
+		
+        // re-writing the JSON file after deleting one of the courses
+        const write = JSON.stringify(myCourses, null, 4);
+        console.log('write json file again');
+        fs.writeFile(filePath, write, 'utf-8', (err, data) => {
+            if (err) {
+                return res.status(500).send(err.message);
+            }
+            console.log('Course was successfully deleted');
+        });
+    });
+});
+
 // environment variable for port
 const port = process.env.PORT || 3000;
 // start listen to application, it takes two arguments; first-> portNumber second=> a function can execute when it starts listening
--- project-planing/development-stragy.md
+++ project-planing/development-stragy.md
@@ -38,7 +38,7 @@ This user story has been developed through a branch called 'getAll'.
 
 This user story has been developed through a branch called 'post'.
 
-> Assign to **Gelila Ali*
+> Assign to **Gelila Ali**
 
 ### Node.js
 
@@ -46,3 +46,18 @@ This user story has been developed through a branch called 'post'.
 - import fs module
 - import joi framework
 - write logic to post a new list on the postman
+
+## 4. Delete course by the given ID
+
+**As a site visitor, I want to be able to delete a specific course by the given ID.**
+
+This user story has been developed through a branch called 'delete'.
+
+> Assign to **Aleksandra**
+
+### Node.js
+
+- Read the file system following the path `${__dirname}/courses.json` which contain the courses in a json format.
+- Search for the required course, if it doesn't -throw an error and return
+- If the course exists - delete it
+- Write the data back to the file system.
