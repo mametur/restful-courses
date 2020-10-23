913d0fdc8cf14df6ca61e1a74fc9995cd5468dd6
Miroslav Veljanoski <veljanoski.miroslav@gmail.com>
2020-10-24 00:38:39 +0200

    Complete get single course route

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 //const courses = require('./data/courses.json')
 // file system module
 const fs = require('fs');
-const Joi = require('joi')
+const Joi = require('joi');
 
 app.use(express.json());
 
 // this method get arguments; first->Url, Second->Callback(request,respond)
 app.get('/', function (req, res) {
-	res.send(`Group1`);
+  res.send(`Group1`);
 });
 
 // read courses.json file
@@ -19,128 +19,137 @@ const filePath = __dirname + '/data/courses.json';
 // callback is added
 
 app.get('/data/courses.json', function (req, res) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    JSON.parse(data);
 
-fs.readFile(filePath, 'utf-8',(err,data)=>{
-	if(err){
-		console.error(err)
-	}
-	JSON.parse(data)
-	
-	//display json data  on the localhost with relative path
-	
-	res.send(data);
-});
-		
+    //display json data  on the localhost with relative path
+
+    res.send(data);
+  });
 });
 
-app.post('/data/courses.json',(req,res)=>{
-	const result = validateCourse(req.body);
-    const { error } = validateCourse(req.body);
-	  if(error) return res.status(400).send(result.error.details[0].message);
-	 
-	 fs.readFile(filePath, 'utf-8',(err,data)=>{
-	if(err){
-		console.error(err)
-	}
-	//JSON.parse(data);
-	let myCourses = JSON.parse(data);
+app.post('/data/courses.json', (req, res) => {
+  const result = validateCourse(req.body);
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(result.error.details[0].message);
 
-const course ={
-		id:myCourses.length+1,
-		name: req.body.name,
-	}
-   myCourses.push(course);
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    //JSON.parse(data);
+    console.log(typeof data);
+    let myCourses = JSON.parse(data);
 
-   const write = JSON.stringify(myCourses,null,4);
+    const course = {
+      id: myCourses.length + 1,
+      name: req.body.name,
+    };
+    myCourses.push(course);
 
-   fs.writeFile(filePath, write,'utf-8', (err,data)=>{
-	  if(err){
-		  res.status(500).send(err.message)
-	  }  
+    const write = JSON.stringify(myCourses, null, 4);
 
-   });
-	 res.send(myCourses);
+    fs.writeFile(filePath, write, 'utf-8', (err, data) => {
+      if (err) {
+        res.status(500).send(err.message);
+      }
+    });
+    res.send(myCourses);
+  });
 });
-		
-})
 
 function validateCourse(course) {
-    const schema =Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    return schema.validate(course);
-    
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(course);
 }
+
+// get single course
+app.get('/data/courses.json/:id', (req, res) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    const allCourses = JSON.parse(data);
+
+    const course = allCourses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found');
+
+    res.send(course);
+  });
+});
 
 // delete by ID
 
 app.delete('/data/courses.json/:id', (req, res) => {
-    // read the file system and look up the course by ID
-    // if doesn't exist - return 404
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            return console.error(err);
-		}
-		
-        let myCourses = JSON.parse(data);
-		const removeCourse = myCourses.find((c) => c.id === parseInt(req.params.id));
-		
-		if (!removeCourse) return res.status(404).send('The course with the given ID was not found');
-        
-        // delete
-        console.log('this will be deleted ', removeCourse);
-        const index = myCourses.indexOf(removeCourse);
-        myCourses.splice(index, 1);
-		res.send(removeCourse);
-		
-        // re-writing the JSON file after deleting one of the courses
-        const write = JSON.stringify(myCourses, null, 4);
-        console.log('write json file again');
-        fs.writeFile(filePath, write, 'utf-8', (err, data) => {
-            if (err) {
-                return res.status(500).send(err.message);
-            }
-            console.log('Course was successfully deleted');
-        });
-    });
-});
+  // read the file system and look up the course by ID
+  // if doesn't exist - return 404
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      return console.error(err);
+    }
 
+    let myCourses = JSON.parse(data);
+    const removeCourse = myCourses.find((c) => c.id === parseInt(req.params.id));
+
+    if (!removeCourse) return res.status(404).send('The course with the given ID was not found');
+
+    // delete
+    console.log('this will be deleted ', removeCourse);
+    const index = myCourses.indexOf(removeCourse);
+    myCourses.splice(index, 1);
+    res.send(removeCourse);
+
+    // re-writing the JSON file after deleting one of the courses
+    const write = JSON.stringify(myCourses, null, 4);
+    console.log('write json file again');
+    fs.writeFile(filePath, write, 'utf-8', (err, data) => {
+      if (err) {
+        return res.status(500).send(err.message);
+      }
+      console.log('Course was successfully deleted');
+    });
+  });
+});
 
 /*  
 PUT method , URL:"/api/courses/id", filePath = `${__dirname}/courses.json`
 read data ==> modify it ==> write it back to the file system
 */
-app.put('/api/courses/:id', (req,res) => {
-       
-	fs.readFile(filePath, 'utf-8', (err, content) => {
-		if(err) {
-			res.status(500).send(err.message);
-			return;
-		}
-		const parsedCourses = JSON.parse(content);
-		const course = parsedCourses.find(c => c.id === parseInt(req.params.id));
-	   if(!course){
-		  res.status(404).send('No course with this id was found');
-		 return;
-		 } 
+app.put('/api/courses/:id', (req, res) => {
+  fs.readFile(filePath, 'utf-8', (err, content) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    }
+    const parsedCourses = JSON.parse(content);
+    const course = parsedCourses.find((c) => c.id === parseInt(req.params.id));
+    if (!course) {
+      res.status(404).send('No course with this id was found');
+      return;
+    }
 
-		 const { error } = validateCourse(req.body)
-		if(error) {
-		res.status(400).send(error.details[0].message);
-		  return;
-		   }
-		   course["name"] = req.body.name;
-		
-		const stringifiedCourses = JSON.stringify(parsedCourses, null, '  ');
-		fs.writeFile(filePath, stringifiedCourses, (err) => {
-			if(err) {
-				res.status(500).send(err.message);
-				return;
-			}
-			res.send(course);
-		});
+    const { error } = validateCourse(req.body);
+    if (error) {
+      res.status(400).send(error.details[0].message);
+      return;
+    }
+    course['name'] = req.body.name;
 
-	});
+    const stringifiedCourses = JSON.stringify(parsedCourses, null, '  ');
+    fs.writeFile(filePath, stringifiedCourses, (err) => {
+      if (err) {
+        res.status(500).send(err.message);
+        return;
+      }
+      res.send(course);
+    });
+  });
 });
 
 // environment variable for port
